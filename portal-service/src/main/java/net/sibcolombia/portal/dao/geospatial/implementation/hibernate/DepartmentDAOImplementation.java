acf2f61277bf47a93df6e8e6044a22e33e55c571
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2012-11-22 16:53:19 +0000

    Addition of department model classes

---
+++ portal-service/src/main/java/net/sibcolombia/portal/dao/geospatial/implementation/hibernate/DepartmentDAOImplementation.java
@@ -0,0 +1,73 @@
+package net.sibcolombia.portal.dao.geospatial.implementation.hibernate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import net.sibcolombia.portal.dao.geospatial.DepartmentDAO;
+import net.sibcolombia.portal.model.geospatial.Department;
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+
+public class DepartmentDAOImplementation extends HibernateDaoSupport implements DepartmentDAO {
+
+  /** The list of currently support locales - dynamically loaded on application start up */
+  protected List<String> supportedLocales = null;
+
+  @SuppressWarnings("unchecked")
+  public List<Character> getDepartmentAlphabet() {
+    HibernateTemplate template = getHibernateTemplate();
+    List<String> departmentChars = (List<String>) template.execute(new HibernateCallback() {
+
+      public Object doInHibernate(Session session) {
+        Query query =
+          session
+            .createSQLQuery("SELECT DISTINCT(SUBSTRING(state_province,1,1)) FROM raw_occurrence_record WHERE raw_occurrence_record.state_province IS NOT NULL AND raw_occurrence_record.deleted IS NULL ORDER BY raw_occurrence_record.state_province ASC");
+        return query.list();
+      }
+    });
+    ArrayList<Character> chars = new ArrayList<Character>();
+    for (String result : departmentChars) {
+      if (StringUtils.isNotEmpty(result))
+        chars.add(new Character(Character.toUpperCase(result.charAt(0))));
+    }
+    Collections.sort(chars);
+    return chars;
+  }
+
+  @SuppressWarnings("unchecked")
+  public List<Department> getDepartmentsFor(final Character theChar) {
+    HibernateTemplate template = getHibernateTemplate();
+    return (List<Department>) template.execute(new HibernateCallback() {
+
+      public Object doInHibernate(Session session) {
+        Query query =
+          session
+            .createQuery("SELECT d.Id AS Id, d.departmentName AS departmentName, d.isoDepartmentCode AS isoDepartmentCode, d.occurrenceCount AS occurrenceCount, d.occurrenceCoordinateCount AS occurrenceCoordinateCount, d.speciesCount AS speciesCount FROM Department d WHERE d.departmentName LIKE :name ORDER BY departmentName ASC");
+        String searchString;
+        if (theChar != null) {
+          searchString = theChar + "%";
+        } else {
+          searchString = "%";
+        }
+        query.setString("name", searchString);
+        query.setCacheable(true);
+        logger.debug("Query debug: " + query.getQueryString());
+        return query.list();
+      }
+    });
+  }
+
+  /**
+   * @param supportedLocales the supportedLocales to set
+   */
+  public void setSupportedLocales(List<String> supportedLocales) {
+    this.supportedLocales = supportedLocales;
+  }
+
+}
---
+++ portal-service/src/main/java/net/sibcolombia/portal/model/geospatial/Department.java
@@ -0,0 +1,246 @@
+/***************************************************************************
+ * Copyright (C) 2012 Sib Colombia.
+ * All Rights Reserved.
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
+package net.sibcolombia.portal.model.geospatial;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+ * Holds details of a Colombian department including statistics data regarding
+ * the number of occurrence records the system holds for a particular
+ * Colombian department.
+ * NOTE:The identifier for this hibernate object isnt the departmentId but the IsoDepartmentCode.
+ * This is to enable joins with the OccurrenceRecord on isoDepartmentCode.
+ * 
+ * @author Valentina Grajales
+ * @hibernate.class
+ *                  table="department"
+ */
+public class Department {
+
+  /**
+   * Department name
+   * protected String isoDepartmentCode;
+   * /** The ISO department Code for this model. Used as the identifier in this model object
+   */
+  protected String isoDepartmentCode;
+  /** The department id - internal id */
+  protected Long departmentId;
+  /** The department name */
+  protected String departmentName;
+
+  /** The number of occurrences records this data resource provides */
+  protected Integer occurrenceCount;
+
+
+  /** The number of occurrences records with geo reference data this data resource provides */
+  protected Integer occurrenceCoordinateCount;
+
+  /** The number of species this data resource provides */
+  protected Integer speciesCount;
+  /** The minimum latitude for this department */
+  protected Float minLatitude;
+  /** The maximum latitude for this department */
+  protected Float maxLatitude;
+  /** The minimum longitude for this department */
+  protected Float minLongitude;
+  /** The maximum longitude for this department */
+  protected Float maxLongitude;
+
+  /**
+   * Returns true if object is an instance of BaseObject and
+   * the identifier value is equal.
+   * Required by ORMS for caching/collections performance.
+   * 
+   * @see java.lang.Object#equals(Object)
+   */
+  @Override
+  public boolean equals(Object object) {
+    if (object instanceof Department) {
+      Department other = (Department) object;
+      if (getIsoDepartmentCode() != null) {
+        return getIsoDepartmentCode().equals(other.getIsoDepartmentCode());
+      } else if (this == object) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @hibernate.property
+   *                     column="id"
+   * @return the id
+   */
+  public Long getDepartmentId() {
+    return departmentId;
+  }
+
+  /**
+   * @return the departmentName
+   */
+  public String getDepartmentName() {
+    return departmentName;
+  }
+
+  /**
+   * @hibernate.id
+   *               generator-class="assigned"
+   *               unsaved-value="null"
+   *               column="iso_department_code"
+   * @return the isoDepartmentCode
+   */
+  public String getIsoDepartmentCode() {
+    return isoDepartmentCode;
+  }
+
+  /**
+   * @hibernate.property
+   *                     column="max_latitude"
+   * @return the maxLatitude
+   */
+  public Float getMaxLatitude() {
+    return maxLatitude;
+  }
+
+  /**
+   * @hibernate.property
+   *                     column="max_longitude"
+   * @return the maxLongitude
+   */
+  public Float getMaxLongitude() {
+    return maxLongitude;
+  }
+
+  /**
+   * @hibernate.property
+   *                     column="min_latitude"
+   * @return the minLatitude
+   */
+  public Float getMinLatitude() {
+    return minLatitude;
+  }
+
+  /**
+   * @hibernate.property
+   *                     column="min_longitude"
+   * @return the minLongitude
+   */
+  public Float getMinLongitude() {
+    return minLongitude;
+  }
+
+  /**
+   * @hibernate.property
+   *                     column="occurrence_coordinate_count"
+   * @return the occurrenceCoordinateCount
+   */
+  public Integer getOccurrenceCoordinateCount() {
+    return occurrenceCoordinateCount;
+  }
+
+  /**
+   * @hibernate.property
+   *                     column="occurrence_count"
+   * @return the occurrenceCount
+   */
+  public Integer getOccurrenceCount() {
+    return occurrenceCount;
+  }
+
+  /**
+   * @hibernate.property
+   *                     column="species_count"
+   * @return the speciesCount
+   */
+  public Integer getSpeciesCount() {
+    return speciesCount;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setDepartmentId(Long id) {
+    this.departmentId = id;
+  }
+
+  /**
+   * @param departmentName the departmentName to set
+   */
+  public void setDepartmentName(String departmentName) {
+    this.departmentName = departmentName;
+  }
+
+  /**
+   * @param isoDepartmentCode the isoDepartmentCode to set
+   */
+  public void setIsoDepartmentCode(String isoDepartmentCode) {
+    this.isoDepartmentCode = isoDepartmentCode;
+  }
+
+  /**
+   * @param maxLatitude the maxLatitude to set
+   */
+  public void setMaxLatitude(Float maxLatitude) {
+    this.maxLatitude = maxLatitude;
+  }
+
+  /**
+   * @param maxLongitude the maxLongitude to set
+   */
+  public void setMaxLongitude(Float maxLongitude) {
+    this.maxLongitude = maxLongitude;
+  }
+
+  /**
+   * @param minLatitude the minLatitude to set
+   */
+  public void setMinLatitude(Float minLatitude) {
+    this.minLatitude = minLatitude;
+  }
+
+  /**
+   * @param minLongitude the minLongitude to set
+   */
+  public void setMinLongitude(Float minLongitude) {
+    this.minLongitude = minLongitude;
+  }
+
+  /**
+   * @param occurrenceCoordinateCount the occurrenceCoordinateCount to set
+   */
+  public void setOccurrenceCoordinateCount(Integer occurrenceCoordinateCount) {
+    this.occurrenceCoordinateCount = occurrenceCoordinateCount;
+  }
+
+  /**
+   * @param occurrenceCount the occurrenceCount to set
+   */
+  public void setOccurrenceCount(Integer occurrenceCount) {
+    this.occurrenceCount = occurrenceCount;
+  }
+
+  /**
+   * @param speciesCount the speciesCount to set
+   */
+  public void setSpeciesCount(Integer speciesCount) {
+    this.speciesCount = speciesCount;
+  }
+
+  /**
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+}
