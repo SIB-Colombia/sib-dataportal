e79c4587ad607a33051969f88ac1e4a118808c3d
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2012-11-26 22:06:01 +0000

    SQL Scripts to initialize and update data portal database.

---
+++ portal-web/db/util/initSIBPortal.sql
@@ -0,0 +1,5 @@
+-- Include department key in lookup_cell_density_type table
+insert into lookup_cell_density_type (cd_key, cd_value) values (7,'department')
+
+-- Include all the departments in department table
+insert INTO department (department.id, department.iso_department_code, department.department_name) VALUES (1, 'CO-AMA','Amazonas'),(2,'CO-ANT','Antioquia'),(3,'CO-ARA','Arauca'),(4,'CO-ATL','Atlántico'),(5,'CO-BOL','Bolívar'),(6,'CO-BOY','Boyacá'),(7,'CO-CAL','Caldas'),(8,'CO-CAQ','Caquetá'),(9,'CO-CAS','Casanare'),(10,'CO-CAU','Cauca'),(11,'CO-CES','Cesar'),(12,'CO-CHO','Chocó'),(13,'CO-COR','Córdoba'),(14,'CO-CUN','Cundinamarca'),(15,'CO-DC','Bogotá Distrito Capital'),(16,'CO-GUA','Guainía'),(17,'CO-GUV','Guaviare'),(18,'CO-HUI','Huila'),(19,'CO-LAG','La Guajira'),(20,'CO-MAG','Magdalena'),(21,'CO-MET','Meta'),(22,'CO-NAR','Nariño'),(23,'CO-NSA','Norte de Santander'),(24,'CO-PUT','Putumayo'),(25,'CO-QUI','Quindío'),(26,'CO-RIS','Risaralda'),(27,'CO-SAN','San Andrés, Providencia y Santa Catalina'),(28,'CO-SAP','Santander'),(29,'CO-SUC','Sucre'),(30,'CO-TOL','Tolima'),(31,'CO-VAC','Valle del Cauca'),(32,'CO-VAU','Vaupés'),(33,'CO-VID','Vichada')
---
+++ portal-web/db/util/processSIBPortal.sql
@@ -0,0 +1,44 @@
+-- Addition by SiB Colombia
+-- This update fills departments of Colombia data
+
+-- populate the centi_cell_density for department
+-- 7 is department lookup_cell_density_type
+select concat('Building centi cells for department: ', now()) as debug;
+insert into centi_cell_density 
+select 8, d.id, cell_id, centi_cell_id, count(oc.id) 
+from occurrence_record oc 
+inner join department d on oc.iso_department_code=d.iso_department_code 
+where oc.centi_cell_id is not null and oc.geospatial_issue=0
+group by 1,2,3,4;
+
+-- clear the cell densities
+-- Query OK, 0 rows affected (0.28 sec)
+select concat('Clearing cells: ', now()) as debug;
+truncate table cell_density;
+
+-- build the cell_densities
+-- Query OK, 13623180 rows affected (18 min 0.63 sec)
+-- Records: 13623180  Duplicates: 0  Warnings: 0
+select concat('Building cell densities: ', now()) as debug;
+insert ignore into cell_density 
+select type,entity_id,cell_id,sum(count)
+from centi_cell_density
+group by 1,2,3;
+
+-- sets the departments count
+select concat('Starting department occurrence count: ', now()) as debug;
+update department d set occurrence_count =
+(select count(id) from occurrence_record o where o.iso_department_code=d.iso_department_code);
+
+-- set occurrence record coordinate count for department table
+select concat('Starting department occurrence coordinate count: ', now()) as debug;
+update department d set occurrence_coordinate_count =   
+(select sum(cd.count) from cell_density cd where cd.entity_id=d.id and cd.type=7);
+
+-- set species count per department
+-- this used to be species and lower concepts as well - changed 12.8.08
+select concat('Starting department species count: ', now()) as debug;
+update department d set species_count = 
+(select count(distinct o.species_concept_id) from occurrence_record o where o.iso_department_code = d.iso_department_code);
+
+-- End of SiB Colombia addition
