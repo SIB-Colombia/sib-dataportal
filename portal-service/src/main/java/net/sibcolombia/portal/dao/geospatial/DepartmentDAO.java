9fd83fd755e8d8b45ecc5c8a171b3ddc522070ae
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2012-11-29 16:45:07 +0000

    Changing department database access object to support department details. Portal-service.

--- portal-service/src/main/java/net/sibcolombia/portal/dao/geospatial/DepartmentDAO.java
+++ portal-service/src/main/java/net/sibcolombia/portal/dao/geospatial/DepartmentDAO.java
@@ -1,6 +1,7 @@
 package net.sibcolombia.portal.dao.geospatial;
 
 import java.util.List;
+import java.util.Locale;
 
 import net.sibcolombia.portal.model.geospatial.Department;
 
@@ -12,11 +13,46 @@ import net.sibcolombia.portal.model.geospatial.Department;
 public interface DepartmentDAO {
 
   /**
+   * Retrieve a list of departments with matching the name supplied.
+   * 
+   * @param nameStub
+   * @param fuzzy
+   * @return SearchResultsDTO containing CountryDTO objects.
+   */
+  public List<Object[]> findDepartmentsFor(String nameStub, boolean fuzzy, boolean anyOccurrence, int startIndex,
+    int maxResults);
+
+  /**
    * Retrieves a list of distinct first characters from the names of departments
    * 
    * @return list of characters
    */
   public List<Character> getDepartmentAlphabet();
+
+  /**
+   * Retrieve counts for departments providing data for the country
+   * 
+   * @param isoDepartmentCode
+   * @param geoRefOnly
+   * @return
+   */
+  public List<Object[]> getDepartmentCountsForDepartment(String isoDepartmentCode, boolean geoRefOnly, Locale locale);
+
+  /**
+   * Retrieve the department for the supplied Id.
+   * 
+   * @param departmentId the internal system id for this department
+   * @return
+   */
+  public Object getDepartmentFor(long departmentId);
+
+  /**
+   * Retrieve the department for Colombia ISO Department code.
+   * 
+   * @param isoDepartmentCode the ISO Department code
+   * @return Object[2], Object[0]=Country, Object[1]=locale specific name/
+   */
+  public Object getDepartmentForIsoDepartmentCode(String isoDepartmentCode);
 
   /**
    * Retrieves all the departments starting with the supplied char or all the departments if char is null.
--- portal-service/src/main/java/net/sibcolombia/portal/dao/geospatial/implementation/hibernate/DepartmentDAOImplementation.java
+++ portal-service/src/main/java/net/sibcolombia/portal/dao/geospatial/implementation/hibernate/DepartmentDAOImplementation.java
@@ -3,11 +3,14 @@ package net.sibcolombia.portal.dao.geospatial.implementation.hibernate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import net.sibcolombia.portal.dao.geospatial.DepartmentDAO;
 import net.sibcolombia.portal.model.geospatial.Department;
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.Hibernate;
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.springframework.orm.hibernate3.HibernateCallback;
 import org.springframework.orm.hibernate3.HibernateTemplate;
@@ -18,6 +21,37 @@ public class DepartmentDAOImplementation extends HibernateDaoSupport implements 
 
   /** The list of currently support locales - dynamically loaded on application start up */
   protected List<String> supportedLocales = null;
+
+  @SuppressWarnings("unchecked")
+  public List<Object[]> findDepartmentsFor(final String nameStub, final boolean fuzzy, final boolean anyOccurrence,
+    final int startIndex, final int maxResults) {
+    return (List<Object[]>) getHibernateTemplate().execute(new HibernateCallback() {
+
+      public Object doInHibernate(Session session) {
+
+        StringBuffer sb = null;
+
+        sb = new StringBuffer("select d from Department d where (d.departmentName like :name");
+        if (anyOccurrence) {
+          sb.append(" or d.departmentName like :anyPartName");
+        }
+        sb.append(")");
+        sb.append(" order by d.departmentName");
+
+        Query query = session.createQuery(sb.toString());
+        String searchString = nameStub;
+        if (fuzzy)
+          searchString = searchString + '%';
+        if (anyOccurrence)
+          query.setString("anyPartName", "% " + searchString);
+
+        query.setString("name", searchString);
+        query.setFirstResult(startIndex);
+        query.setMaxResults(maxResults);
+        return query.list();
+      }
+    });
+  }
 
   @SuppressWarnings("unchecked")
   public List<Character> getDepartmentAlphabet() {
@@ -38,6 +72,75 @@ public class DepartmentDAOImplementation extends HibernateDaoSupport implements 
     }
     Collections.sort(chars);
     return chars;
+  }
+
+  @SuppressWarnings("unchecked")
+  public List<Object[]> getDepartmentCountsForDepartment(final String isoDepartmentCode, final boolean geoRefOnly,
+    final Locale locale) {
+    HibernateTemplate template = getHibernateTemplate();
+    return (List<Object[]>) template.execute(new HibernateCallback() {
+
+      public Object doInHibernate(Session session) {
+        StringBuffer sb = new StringBuffer("select dp.iso_country_code as the_iso_country_code, cn.name as cn_name,");
+        if (geoRefOnly) {
+          sb.append("  sum(rc.occurrence_coordinate_count) as the_count from resource_country rc"
+            + " inner join data_resource dr on rc.data_resource_id=dr.id "
+            + " inner join data_provider dp on dr.data_provider_id=dp.id "
+            + " inner join country_name cn on dp.iso_country_code=cn.iso_country_code" + " where ");
+        } else {
+          sb.append("  sum(rc.count) as the_count from resource_country rc"
+            + " inner join data_resource dr on rc.data_resource_id=dr.id "
+            + " inner join data_provider dp on dr.data_provider_id=dp.id "
+            + " inner join country_name cn on dp.iso_country_code=cn.iso_country_code" + " where ");
+        }
+        if (isoDepartmentCode != null) {
+          sb.append(" rc.iso_country_code=:isoCountryCode");
+        }
+
+        if (geoRefOnly) {
+          sb.append(" and rc.occurrence_coordinate_count>0");
+        }
+
+        sb.append(" and dr.deleted is null");
+
+        sb.append(" group by cn_name");
+        SQLQuery query = session.createSQLQuery(sb.toString());
+        if (isoDepartmentCode != null) {
+          query.setParameter("isoCountryCode", isoDepartmentCode);
+          query.addScalar("the_iso_country_code", Hibernate.STRING);
+          query.addScalar("cn_name", Hibernate.STRING);
+          query.addScalar("the_count", Hibernate.INTEGER);
+        }
+        query.setCacheable(true);
+        logger.debug("query is: " + sb.toString());
+        return query.list();
+      }
+    });
+  }
+
+  /**
+   * @see net.sibcolombia.portal.dao.geospatial.DepartmentDAO#getDepartmentFor(long)
+   */
+  public Object getDepartmentFor(final long departmentId) {
+    return getHibernateTemplate().execute(new HibernateCallback() {
+
+      public Object doInHibernate(Session session) {
+        Query query = session.createQuery("from Department d where d.id=:departmentId");
+        query.setLong("departmentId", departmentId);
+        return query.uniqueResult();
+      }
+    });
+  }
+
+  public Object getDepartmentForIsoDepartmentCode(final String isoDepartmentCode) {
+    return getHibernateTemplate().execute(new HibernateCallback() {
+
+      public Object doInHibernate(Session session) {
+        Query query = session.createQuery("from Department d where d.isoDepartmentCode=:isoDepartmentCode");
+        query.setString("isoDepartmentCode", isoDepartmentCode);
+        return query.uniqueResult();
+      }
+    });
   }
 
   @SuppressWarnings("unchecked")
