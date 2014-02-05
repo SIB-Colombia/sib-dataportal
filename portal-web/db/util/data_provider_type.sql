24da5d1f00339808f91863aa07ec8b1b2372e121
mcubillos <cubillos.maria0@gmail.com>
2014-02-05 15:48:02 -0500

    Set "SiB Colombia" as "Repatriados"

--- portal-web/db/util/data_provider_type.sql
+++ portal-web/db/util/data_provider_type.sql
@@ -6,3 +6,4 @@ UPDATE data_provider SET type = "Instituto de investigación" WHERE name in ("In
 UPDATE data_provider SET type = "ONG" WHERE name in ("Asociación para el estudio y conservación de las aves acuáticas en Colombia","Asociación de Becarios del Casanare - ABC", "Asociación GAICA","Asociación Selva","Fundación Alma","Fundación Pro-Sierra Nevada de Santa Marta","Jardín Botánico del Quindío","WCS Colombia");
 UPDATE data_provider SET type = "Red temática/regional" WHERE name in ("Red Nacional de Observadores de Aves (RNOA)");
 UPDATE data_provider SET type = "Universidad" WHERE name in ("Universidad de Antioquia","Instituto de Ciencias Naturales","Universidad de La Salle","Universidad de los Andes","Universidad de Nariño","Universidad del Magdalena","Universidad del Valle");
+UPDATE data_provider SET type = "Repatriados" WHERE name in ("SiB Colombia");
